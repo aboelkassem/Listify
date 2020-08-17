78e0ec1d6a9a738cde9af95cb934f4bf8e8d093b
Mohamed Abdelrahman <42910789+Aboelkassem@users.noreply.github.com>
2020-08-18 01:36:33 +0200

    full functionalities of rooms and room components

--- Listify/src/app/app-routing.module.ts
+++ Listify/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
-import { AccountComponent } from './account/account.component';
+import { CurrenciesComponent } from './currencies/currencies.component';
 import { HomeComponent } from './home/home.component';
-import { YoutubeComponent } from './youtube/youtube.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { AccountComponent } from './account/account.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoomComponent } from './room/room.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'youtube', component: YoutubeComponent},
+  {path: 'room', component: RoomComponent},
+  {path: 'rooms', component: RoomsComponent},
   {path: 'account', component: AccountComponent},
+  {path: 'currencies', component: CurrenciesComponent},
 ];
 
 @NgModule({
